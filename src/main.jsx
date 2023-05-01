6de703d1b410df8f014edd0eac2b3b381a8f9cc2
Shahinur2010 <shahinurmony4@gmail.com>
2023-05-02 01:13:41 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div className='bg-slate-300'>Hello world! <button className="btn btn-error">Button</button></div>
+    ,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
