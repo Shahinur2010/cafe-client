e18008305b8c45521d56f3b04cb67bfa5e571a56
Shahinur2010 <shahinurmony4@gmail.com>
2023-05-03 02:33:07 +0600

    README file added

--- src/components/shared/Footer/Footer.jsx
+++ src/components/shared/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div className='absolute'>
-            <h2 className='mb-0 mt-auto'>copyrighy@japanesefodigecafe reserved</h2>
+            <h2>copyrighy@japanesefodigecafe reserved</h2>
         </div>
     );
 };
